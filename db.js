25c12e63c270ba18c3295712611d8c375c02c440
minhee 318 <alsgml0867@naver.com>
2020-02-29 06:51:02 +0900

    fill out form
    study mixin
    it's getting confused.🤦‍♀️

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "밥 도둑 셋",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "minhee",
+      email: "alsgml0867@naver.com"
+    }
+  },
+  {
+    id: 234565,
+    title: "고추장 계란 비빔밥",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "minhee",
+      email: "alsgml0867@naver.com"
+    }
+  },
+  {
+    id: 778899,
+    title: "악마의 수프",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "minhee",
+      email: "alsgml0867@naver.com"
+    }
+  },
+  {
+    id: 767545,
+    title: "답변입니다.",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "minhee",
+      email: "alsgml0867@naver.com"
+    }
+  }
+];
