d5c61ad45603309c542ac6d58adff83af44aea40
minhee 318 <alsgml0867@naver.com>
2020-03-03 04:38:51 +0900

    Connected MongoDB
    install mongoose,dotenv

--- db.js
+++ db.js
@@ -1,54 +1,16 @@
-export const videos = [
-  {
-    id: 324393,
-    title: "밥 도둑 셋",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "minhee",
-      email: "alsgml0867@naver.com"
-    }
-  },
-  {
-    id: 234565,
-    title: "고추장 계란 비빔밥",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "minhee",
-      email: "alsgml0867@naver.com"
-    }
-  },
-  {
-    id: 778899,
-    title: "악마의 수프",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "minhee",
-      email: "alsgml0867@naver.com"
-    }
-  },
-  {
-    id: 767545,
-    title: "답변입니다.",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "minhee",
-      email: "alsgml0867@naver.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✔ Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+db.once("open", handleOpen);
+db.on("error", handleError);
